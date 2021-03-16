92e232f03fcfd2e65fe102d39b9d777dd52617de
Kevin Miller <millerk@reninc.com>
2021-03-16 06:11:22 -0400

    update pp and add examples

---
+++ code/doSomethingRail.ts
@@ -0,0 +1,37 @@
+function validateAddress(address) {}
+function createAddressEntity(addressEntity) {}
+function sendToAddress(addressEntity) {}
+
+interface Object {
+  pipe<TIn, TOut>(fn: (x: TIn) => TOut): TOut;
+}
+
+interface Function {
+  compose<TStart, TIn, TOut>(fn: (x: TIn) => TOut): (t: TStart) => TOut;
+}
+
+Object.prototype.pipe = function (fn) {
+  return fn(this);
+};
+
+function sendMail(address) {
+  const validatedAddress = validateAddress(address);
+  const addressEntity = createAddressEntity(validatedAddress);
+  const result = sendToAddress(addressEntity);
+  return result;
+}
+
+function sendMailFn(address) {
+  return address
+    .pipe(validateAddress)
+    .pipe(createAddressEntity)
+    .pipe(sendToAddress);
+}
+
+const sendEmail = validateAddress
+  .compose(createAddressEntity)
+  .compose(sendToAddress);
+
+/*const sendEmail = function (address) {
+    return sendToAddress(createAddressEntity(validateAddress(address)));
+}*/
